804c75ed46b2fdedbb72834a4c70df01f62b9a46
Sunway12 <sunwayholidays12@gmail.com>
2025-08-03 13:33:05 +0530

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My App</title>
+    <style>
+        body { font-family: sans-serif; display: grid; place-content: center; height: 100vh; margin: 0; background-color: #f0f2f5; }
+        h1 { color: #1d2129; }
+    </style>
+</head>
+<body>
+    <h1>Hello from my DigitalOcean Droplet! 👋</h1>
+</body>
+</html>
